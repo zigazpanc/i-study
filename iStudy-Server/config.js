cbdc75b79b9438a07ceb9d42cd663574db1bc352
zigazpanc <54882393+zigazpanc@users.noreply.github.com>
2020-05-02 20:19:09 +0200

    Lil update

--- iStudy-Server/config.js
+++ iStudy-Server/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     hostUrl: 'http://localhost:5000',
     clientUrl: 'http://localhost:3000',
-    database: 'mongodb://localhost:27017/iStudy',
+    database: 'mongodb://localhost:27017/i-study',
     secret: "$@%#@$@!%&$#&@$",
     port: 5000
 };
